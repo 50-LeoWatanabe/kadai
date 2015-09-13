4297f8c766abef84d99a61b9f2b338045dbdcb08
Leo <sugarray13@gmail.com>
2015-09-13 15:29:14 +0900

    no message

--- 6/index.php
+++ 6/index.php
@@ -32,14 +32,14 @@ $pdo = new PDO("mysql:host=localhost;dbname=cs_academy;charset=utf8", "root", ""
 // $sql = "SELECT name, email FROM enq";
 // $sql = "SELECT * FROM enq where id = 1";
 // $sql = "SELECT * FROM enq LIMIT 3";
- $sql = "SELECT * FROM news ORDER BY update_date DESC LIMIT 5";
+ $sql = "SELECT * FROM enq ORDER BY update_date DESC LIMIT 5";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 //var_dump($results);
 foreach($results as $row) {
 //	var_dump($row);
-	echo $row["create/date"] . "<br>";
+	echo $row["create_date"] . "<br>";
 	echo $row["news_title"] . "<br>";
 }
 $pdo = null;
